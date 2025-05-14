1a0ea450ce036f64dc95a6d98d170cc4106322bf
marty957 <fantuzzimartina@yahoo.it>
2025-05-14 15:19:00 +0200

    modale per aggiungere ditta

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
+import ModalAddFirm from "./ModalAddFirm";
 
 function Herosection() {
+  const [modalShow, setModalShow] = useState(false);
   return (
     <>
       <Container className="sfondo">
         <Container className="position-relative vibrate-1" style={{ height: "90vh" }}>
-          <button className="position-absolute top-50 start-50 translate-middle p-3 button_two vibrate-1">ADD YUOR FIRM</button>
+          <button className="position-absolute top-50 start-50 translate-middle px-5 py-4 button_two vibrate-1" onClick={() => setModalShow(true)}>
+            ADD YUOR FIRM
+          </button>
         </Container>
       </Container>
+      <ModalAddFirm show={modalShow} onHide={() => setModalShow(false)} />
     </>
   );
 }
